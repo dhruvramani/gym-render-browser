4227f970817552039d4fef270d5c9715512052bf
dhruvramani <dhruvramani98@gmail.com>
2020-09-10 17:33:55 +0530

    v0.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
      name='render_browser',  
-     version='0.2',
+     version='0.3',
      author="Dhruv Ramani",
      author_email="dhruvramani98@gmail.com",
      description="Render gym environments to a web browser.",
